501f1506a27fd0ac88e6906c6e34813758a04084
jaredrichards <jdrichards027@gmail.com>
2023-01-19 17:38:25 -0500

    Completed Code to Create .exe

--- gui.py
+++ gui.py
@@ -1,17 +1,24 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 sg.theme('Material1')
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add", size=10)
+add_button = sg.Button(size=4, image_source='add.png',
+                       tooltip='Add To-Do', key='Add')
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button('Edit')
-complete_button = sg.Button('Complete')
+complete_button = sg.Button(size=6, image_source='complete.png',
+                            tooltip='Complete', key='Complete')
 exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
